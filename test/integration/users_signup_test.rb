9f1d108bfb6c2cbd3d081797afce4a7499d5cfdd
Yu Abe <y0u4a2b6e0@gmail.com>
2019-07-08 15:40:13 +0900

    Add account activation

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -6,6 +6,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
 
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
   test "invalid signup information" do
     get signup_path
     #無効なユーザーを作ろうとしてもユーザーが増えない
@@ -22,7 +26,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'form[action="/signup"]'
   end
 
-  test "valid signup information" do
+  test "valid signup information with account activation" do
     get signup_path
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name: "Example User",
@@ -30,9 +34,26 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                 password: "password",
                                 password_confirmation: "password"}}
     end
+    assert_equal 1,ActionMailer::Base.deliveries.size
+    #assignsを使うことで、アクション内のインスタンス変数(@変数)にアクセス可能になる.
+    #アクセスする時は:変数として ハッシュのように扱う
+    #メールが送られた数が1つであることを確認
+    user = assigns(:user)
+    assert_not user.activated?
+    #有効化してない状態でログインしてみる
+    log_in_as(user)
+    assert_not is_logged_in?
+    #有効化トークンが不正な場合
+    get edit_account_activation_path("invalid token", email: user.email)
+    assert_not is_logged_in?
+    #トークンは正しいがメールアドレスが無効な場合
+    get edit_account_activation_path(user.activation_token, email: "wrong")
+    assert_not is_logged_in?
+    #有効化トークンが正しい場合
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
     follow_redirect!
     assert_template 'users/show'
-    assert_not flash.empty?
     assert is_logged_in?
   end
 
