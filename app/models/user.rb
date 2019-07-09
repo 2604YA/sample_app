da3d4f81db5414282c0ea98164e001bc2f22fc9b
Yu Abe <y0u4a2b6e0@gmail.com>
2019-07-09 20:11:05 +0900

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   #仮想の属性 :remember_digestにあとで代入する
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
   validates :name, presence: true, length: {maximum: 50}
@@ -35,6 +35,7 @@ class User < ApplicationRecord
     return false if digest.nil?
     BCrypt::Password.new(digest).is_password?(token)
   end
+  #is_password? は == と同じ働き
 
   def forget
     update_attribute(:remember_digest, nil)
@@ -48,9 +49,29 @@ class User < ApplicationRecord
   #selfの呼び出しは必須ではない。Userモデル内なのでどのテーブルかわかってる？
 
   def send_activation_email
+    #モデルでは変数 user を定義していないので self
     UserMailer.account_activation(self).deliver_now
   end
-  #モデルでは変数 user を定義していないので self
+
+  #パスワード再設定ののメールを送信する
+  def create_reset_digest
+    self.reset_token = User.new_token
+    # update_attribute(:reset_digest, User.digest(reset_token))
+    # update_attribute(:reset_sent_at, Time.zone.now)
+    update_columns(reset_digest: User.digest(reset_token),
+                   reset_sent_at: Time.zone.now )
+  end
+
+  #パスワード再設定のメールを送信する
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+    #2 hours ago より前に送られてるか？=２時間以上経過している
+  end
+
 
   private
 
