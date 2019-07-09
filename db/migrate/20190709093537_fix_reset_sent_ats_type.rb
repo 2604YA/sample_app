8a7b382bc5824b85b661acd1460eb60cfddbde6e
Yu Abe <y0u4a2b6e0@gmail.com>
2019-07-09 21:41:00 +0900

    unactivated dome migration files

--- db/migrate/20190709093537_fix_reset_sent_ats_type.rb
+++ db/migrate/20190709093537_fix_reset_sent_ats_type.rb
@@ -1,6 +1,6 @@
-class FixResetSentAtsType < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :users, :reset_sent_at, :datatime
-    add_column :users, :reset_sent_at, :datetime
-  end
-end
+# class FixResetSentAtsType < ActiveRecord::Migration[5.1]
+#   def change
+#     remove_column :users, :reset_sent_at, :datatime
+#     add_column :users, :reset_sent_at, :datetime
+#   end
+# end
