6d3ca41ad5efc0e5180e66e66e4c6ee6ceb20a64
Yu Abe <y0u4a2b6e0@gmail.com>
2019-07-10 15:45:33 +0900

    deleted some migration files and put a correct one.App should work on heroku

--- db/migrate/20190709093537_fix_reset_sent_ats_type.rb
+++
@@ -1,6 +0,0 @@
-# class FixResetSentAtsType < ActiveRecord::Migration[5.1]
-#   def change
-#     remove_column :users, :reset_sent_at, :datatime
-#     add_column :users, :reset_sent_at, :datetime
-#   end
-# end
