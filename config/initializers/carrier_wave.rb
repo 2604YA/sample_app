80df0e2b616359b2d5b3c797655b2f5bc8d42c86
Yu Abe <y0u4a2b6e0@gmail.com>
2019-07-14 21:36:28 +0900

    fixed

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,9 +4,9 @@ if Rails.env.production?
       # Amazon S3用の設定
       :provider              => 'AWS',
       :region                => ENV['S3_ap-northeast-1'],     # 例: 'ap-northeast-1'
-      :aws_access_key_id     => ENV['S3_AKIAV54SNO7CU4G6THWS'],
-      :aws_secret_access_key => ENV['S3_zZZsoVVpR+XYnivYtMbmMu18AFe2xQ6pv7HALFyV']
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['S3_ya426']
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
