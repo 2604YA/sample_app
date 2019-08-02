0b0b7ad98dd541c86e4961a5a90c91c84ceb75ea
Yu Abe <y0u4a2b6e0@gmail.com>
2019-08-02 20:39:25 +0900

    Add user following

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -96,6 +96,24 @@ class UserTest < ActiveSupport::TestCase
     assert_not michael.following?(archer)
   end
 
+  test "feed should have the right posts" do
+    michael = users(:michael)
+    archer = users(:archer)
+    lana = users(:lana)
+    #assert following user's posts
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+    #assert my posts
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+    #assert there is no unfollowed user's post
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
+    end
+  end
+
 
 end
 
